b2579a410b0c52d04a676f382b78c5e1288e982d
PengJiyuan <731401082@qq.com>
2018-05-14 10:08:30 +0800

    export components subs

--- components/index.js
+++ components/index.js
@@ -1,7 +1,18 @@
+// main layout
 export main from './main/index.jsx';
+// main pop
 export modalV2 from './modal_v2/index.jsx';
+// pop plus
 export modalPlus from './modal_plus/index.jsx';
+// pop delete
 export modalDelete from './modal_delete/index.jsx';
+// pop alert
 export modalAlert from './modal_alert/index';
+// side menu component
 export sideMenu from './side_menu/index.jsx';
+
+/* subs component */
+export * as subs from './modal_v2/subs/export';
+
+// react-router history state
 export history from './history';
